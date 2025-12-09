e7e2c11352310403c1e5a47080e1af0a793f65ad
achrafrf <itsachrafrafiq@gmail.com>
2025-12-09 17:07:42 +0100

    update better-auth version 3

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
 import path from "node:path";
 
 const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
 
-const nextConfig: NextConfig = {
+// حيدنا ": NextConfig" من هنا باش TypeScript يسكت
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -16,8 +16,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // هاد السطر هو اللي حيدناه
-  // outputFileTracingRoot: path.resolve(__dirname, '../../'), 
   
   typescript: {
     ignoreBuildErrors: true,
